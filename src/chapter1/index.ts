c40136b3671cc8e34e2e39d19c91b99c0675a082
사용자이름 <>
2022-03-28 18:30:21 +0900

    .

--- src/chapter1/index.ts
+++ src/chapter1/index.ts
@@ -1,5 +1,76 @@
 class Invitation {
   private when: number = 0;
-} 
+}
 
-export default Invitation;
+class Ticket{
+  private fee: number =0;
+  
+  getFee():number {
+    return this.fee;
+  }
+}
+
+class Bag{
+  private amount:number =0;
+  private invitation:boolean=true;
+  private ticket:number =0;
+
+  hasInvitation():boolean {
+    return true;
+  }
+
+  hasTicket():boolean {
+    return true;
+  }
+
+  setTicket(ticket:number):void{
+    this.ticket = ticket;
+  }
+
+  minusAmount(amount:number):void{
+    this.amount-=amount;
+  }
+
+  plusAmount(amount:number):void{
+    this.amount+=amount;
+  }
+
+  Bag(amount:number,invitation?:boolean){
+    this.invitation = invitation;
+    this.amount = amount;
+  }
+
+}
+
+class Audience{
+  private bag:object;
+
+  Audience(bag:object){
+    this.bag=bag;
+  }
+
+  getBag():object{
+    return this.bag;
+  }
+}
+
+
+class TicketOffice{
+  private amount:number = 0;
+  private ticket:object[]=[];
+
+  TicketOffice(amount:number, ticket:object[]){
+    this.amount=amount;
+    this.ticket = ticket;
+    
+  }
+}
+
+
+
+
+
+
+
+export {Invitation, Ticket, Bag, Audience,TicketOffice}; 
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,67 @@
 
-import Invitation from './chapter1';
+import { setWith } from 'lodash';
+import {Invitation,Ticket,Bag,Audience,TicketOffice} from './chapter1';
 
-const a = new Invitation(); 
+// const a = new Invitation(); 
+// const b = new Ticket();
+// const c = new Bag();
+// const d = new Audience();
+// const e = new TicketOffice();
 
-console.log(a);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// interface Circle {
+//   width : number;
+// }
+
+// class circle {
+//   private attrs : Circle;
+//   constructor(attrs: Circle) {
+//     this.attrs = attrs;
+//   }
+//   getW() : number {
+//     return (this.attrs.width * this.attrs.width) * 3.14;
+//   }
+
+//   setWidth(width:number){
+//     this.attrs.width = width;
+//   }
+// }
+
+// const x = 100;
+// const t = new circle({width : x} as Circle);
+
+// t.setWidth(50);
+
+// console.log(t.getW());
+
+
+interface ISquare {
+  width : number;
+}
+
+class Square {
+  private attrs : ISquare;
+
+  constructor(attrs:ISquare){
+    this.attrs = attrs;
+  }
+
+  getS() : number {
+    return this.attrs.width * this.attrs.width;
+  }
+
+  setWidth(width:number) {
+    this.attrs.width = width;
+  }
+}
+
+let x = 10;
+
+const s = new Square({width : x} as ISquare);
+s.setWidth(5);
+console.log(s.getS());
