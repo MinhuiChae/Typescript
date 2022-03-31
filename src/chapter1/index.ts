028027ecea3f8c8dbd6416951a297602522df4dc
사용자이름 <>
2022-03-31 17:57:15 +0900

    add study2

--- src/chapter1/index.ts
+++ src/chapter1/index.ts
@@ -88,6 +88,7 @@ class TicketSeller {
     return this.ticketOffice;
   }
   
+  
 }
 
 class Theater {
--- src/study/index.ts
+++ src/study/index.ts
@@ -1,90 +1,98 @@
-// interface Window {
-//   shapeModule : any;
-// }
+interface Window {
+  shapeModule : any;
+}
 
-// interface IWidthHeight {
-//   width: number,
-//   height: number
-// }
+interface IWidthHeight {
+  width: number,
+  height: number
+}
 
-// interface IShapeWidthHeight extends IWidthHeight{
-//   shape: string
-// }
+interface IShapeWidthHeight extends IWidthHeight{
+  shape: string
+}
 
-// abstract class Shape {
-//   width: number;
-//   height: number;
+abstract class Shape {
+  width: number;
+  height: number;
 
-//   constructor(info: IShapeWidthHeight) {
-//     this.width = info.width;
-//     this.height = info.height;
-//   }
+  constructor(info: IShapeWidthHeight) {
+    this.width = info.width;
+    this.height = info.height;
+  }
 
-//   abstract getWeight(): number;
-// } 
+  abstract getWeight(): number;
+} 
 
-// class getCircleWeight extends Shape{
-//   constructor(info: IShapeWidthHeight) {
-//     super(info);
-//   }
+class getCircleWeight extends Shape{
+  constructor(info: IShapeWidthHeight) {
+    super(info);
+  }
 
-//   getWeight(): number {
-//     return this.width * this.width * 3.14;
-//   }
-// }
+  getWeight(): number {
+    return this.width * this.width * 3.14;
+  }
+}
 
-// class getTriangleWeight extends Shape{
-//   constructor(info: IShapeWidthHeight) {
-//     super(info);
-//   }
+class getTriangleWeight extends Shape{
+  constructor(info: IShapeWidthHeight) {
+    super(info);
+  }
 
-//   getWeight(): number {
-//     return (this.width * this.height)/2;
-//   }
-// }
+  getWeight(): number {
+    return (this.width * this.height)/2;
+  }
+}
 
-// class getSquareWeight extends Shape{
-//   constructor(info: IShapeWidthHeight) {
-//     super(info);
-//   }
+class getSquareWeight extends Shape{
+  constructor(info: IShapeWidthHeight) {
+    super(info);
+  }
 
-//   getWeight(): number {
-//     return this.width * this.height;
-//   }
-// }
+  getWeight(): number {
+    return this.width * this.height;
+  }
+}
 
-// class ShapeModuleFactory {
-//   shapeInfo : Shape;
+class ShapeModuleFactory {
+  shapeInfo : Shape;
 
-//   constructor(info: IShapeWidthHeight) {
-//     if(info.shape == 'circle') {
-//       this.shapeInfo = new getCircleWeight(info);
-//     }else if(info.shape == 'triangle') {
-//       this.shapeInfo = new getTriangleWeight(info);
-//     }else if(info.shape == 'square') {
-//       this.shapeInfo = new getSquareWeight(info);
-//     }
-//   }
+  constructor(info: IShapeWidthHeight) {
+    if(info.shape == 'circle') {
+      this.shapeInfo = new getCircleWeight(info);
+    }else if(info.shape == 'triangle') {
+      this.shapeInfo = new getTriangleWeight(info);
+    }else if(info.shape == 'square') {
+      this.shapeInfo = new getSquareWeight(info);
+    }
+  }
 
-//   getWeight(): number {
-//     return this.shapeInfo.getWeight();
-//   }
-// }
+  getWeight(): number {
+    return this.shapeInfo.getWeight();
+  }
+}
 
-// ((w: Window) => {
-//   const getShapeController = {
-//     getWeight: (info: IShapeWidthHeight) => {
-//       return new ShapeModuleFactory(info).getWeight();
-//     }
-//   }
+((w: Window) => {
+  const getShapeController = {
+    getWeight: (info: IShapeWidthHeight) => {
+      return new ShapeModuleFactory(info).getWeight();
+    }
+  }
 
-//   w.shapeModule = getShapeController;
-// })(window)
-
-
-import {Invitation,Ticket,Bag,Audience,TicketOffice} from '../chapter1';
+  w.shapeModule = getShapeController;
+})(window)
 
 
-const a = new Invitation();
+// import {Invitation,Ticket,Bag,Audience,TicketOffice} from '../chapter1';
 
-console.log(a);
+
+// const a = new Invitation();
+// const b = new Ticket();
+// const c = new Bag();
+// const d = new Audience();
+// const e = new TicketOffice();
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
