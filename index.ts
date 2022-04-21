6e157498fcc40a50a52beb859504b73d5200ed6e
사용자이름 <>
2022-04-21 16:50:09 +0900

    study

--- index.ts
+++ index.ts
@@ -1,25 +1,11 @@
-import TimeOfDayDiscountCall from './src/chapter14/timeOfDayDiscountPolicy';
-import { Money} from './src/chapter14/phone';
-import PersonalFeeCalculate from './src/chapter14/personalCalculateFee';
+function solution2(n: number, lost: number[], reserve: number[]) {
+  return n - lost.filter(a => {
+    const b = reserve.find(r => Math.abs(r-a) <= 1)
+    if(!b) return true
+    reserve = reserve.filter(r => r !== b)
+}).length
+}
 
 
 
-const date = new Date(2021,0,1,21);
-const date3 = new Date(2021,0,3,10);
-const date4 = new Date(2021,1,3,10);
-const date5 = new Date(2021,1,13,10);
-
-const time = new TimeOfDayDiscountCall(date, date3, new Money(10), new Money(20), 10);
-const time2 = new TimeOfDayDiscountCall(date4, date5, new Money(10), new Money(20), 10);
-
-const personal = new PersonalFeeCalculate([time, time2]);
-
-console.log(personal.calculatePersonalFee());
-console.log(time.calAllDate());
-console.log(time2.calAllDate());
-
-
-
-
-
-
+console.log(solution2(5, [2,4], [1, 3, 5]));
--- src/study3/index.ts
+++ src/study3/index.ts
@@ -26,103 +26,104 @@
 
 // 2번 문제 : 로또당첨
 
-function solution(lottos: number[], win_nums: number[]) {
-  let list:number[] = [];
-  let score = new Array();
-  let count: number = 0;
-  lottos.map(a => {
-    if(a === 0) {
-      count ++;
-    }
-  })
+// function solution(lottos: number[], win_nums: number[]) {
+//   let list:number[] = [];
+//   let score = new Array();
+//   let count: number = 0;
+//   lottos.map(a => {
+//     if(a === 0) {
+//       count ++;
+//     }
+//   })
 
-  win_nums.map((a: number) => {
-    lottos.map((b: number) => {
-      if(a == b) {
-        list.push(a);
-      }
-    })
-  })
+//   win_nums.map((a: number) => {
+//     lottos.map((b: number) => {
+//       if(a == b) {
+//         list.push(a);
+//       }
+//     })
+//   })
 
-  const c = list.length;
+//   const c = list.length;
 
-  for(let i=1; i<=count; i++) {
-    let a:number = Math.floor(Math.random()*44+1);
+//   for(let i=1; i<=count; i++) {
+//     let a:number = Math.floor(Math.random()*44+1);
     
-    if(lottos.indexOf(a) === -1) {
-      lottos.push(a);
+//     if(lottos.indexOf(a) === -1) {
+//       lottos.push(a);
 
-      if(win_nums.indexOf(a) >0) {
-        list.push(a);
-      }
+//       if(win_nums.indexOf(a) >0) {
+//         list.push(a);
+//       }
 
-    }else i--
+//     }else i--
     
-  }
+//   }
 
-  let index = lottos.indexOf(0);
+//   let index = lottos.indexOf(0);
 
-  if(index > -1) {
-  lottos.splice(index, count);
-  }
-  const f = list.length; 
+//   if(index > -1) {
+//   lottos.splice(index, count);
+//   }
+//   const f = list.length; 
 
 
-  switch(f) {
-    case 6:
-      score[0] = 1;
-      break;
-    case 5:
-      score[0] = 2;
-      break;
-    case 4:
-      score[0] = 3;
-      break;
-    case 3:
-      score[0] = 4;
-      break;
-    case 2:
-      score[0] = 5;
-      break;
-    default:
-      score[0] = 6;
-      break;
-  }
+//   switch(f) {
+//     case 6:
+//       score[0] = 1;
+//       break;
+//     case 5:
+//       score[0] = 2;
+//       break;
+//     case 4:
+//       score[0] = 3;
+//       break;
+//     case 3:
+//       score[0] = 4;
+//       break;
+//     case 2:
+//       score[0] = 5;
+//       break;
+//     default:
+//       score[0] = 6;
+//       break;
+//   }
 
-  switch(c) {
-    case 6:
-      score[1] = 1;
-      break;
-    case 5:
-      score[1] = 2;
-      break;
-    case 4:
-      score[1] = 3;
-      break;
-    case 3:
-      score[1] = 4;
-      break;
-    case 2:
-      score[1] = 5;
-      break;
-    default:
-      score[1] = 6;
-      break;
-  }
+//   switch(c) {
+//     case 6:
+//       score[1] = 1;
+//       break;
+//     case 5:
+//       score[1] = 2;
+//       break;
+//     case 4:
+//       score[1] = 3;
+//       break;
+//     case 3:
+//       score[1] = 4;
+//       break;
+//     case 2:
+//       score[1] = 5;
+//       break;
+//     default:
+//       score[1] = 6;
+//       break;
+//   }
 
   
-  console.log(list);
-  console.log(lottos);
-  console.log(c);
-  console.log(f);
-  return score;
-}
+//   console.log(list);
+//   console.log(lottos);
+//   console.log(c);
+//   console.log(f);
+//   return score;
+// }
 
 
-const a = solution([44,1,0,45,31,25] , [31,10,45,1,6,19]);
+// const a = solution([44,1,0,45,31,25] , [31,10,45,1,6,19]);
 
-console.log(a);
+// console.log(a);
 
 
-// 3번문제 : 숫자 문자열과 영단어
+// 3번문제 : 체육복
+
 
